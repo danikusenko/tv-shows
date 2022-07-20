84d4c472587260bac4b43170877815e5443401e1
danikusenko <danilausa99@gmail.com>
2022-07-20 23:54:05 +0300

    add title and description

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Stack, Text, Link, FontWeights, IStackTokens, IStackStyles, ITextStyles } from '@fluentui/react';
 import logo from './logo.svg';
 import './App.css';
+import 'office-ui-fabric-react/dist/css/fabric.css';
 
 const boldStyle: Partial<ITextStyles> = { root: { fontWeight: FontWeights.semibold } };
 const stackTokens: IStackTokens = { childrenGap: 15 };
@@ -16,32 +17,13 @@ const stackStyles: Partial<IStackStyles> = {
 
 export const App: React.FunctionComponent = () => {
   return (
-    
     <Stack className='board'>
-      <Stack horizontalAlign='start'>
+      <div className="ms-Grid" dir="ltr">
         <Text variant="xxLargePlus" styles={boldStyle}>TV Shows</Text>
+      </div>
+      <div className="ms-Grid-col ms-sm6 ms-xl6">
         <Text variant="large">These days, the small screen has some very big things to offer. From sitcoms to dramas to travel and talk shows, these are all the best programs on TV.</Text>
-      </Stack>
-      <Stack horizontalAlign="center" verticalAlign="center" verticalFill styles={stackStyles} tokens={stackTokens}>
-      
-      <Text variant="large" styles={boldStyle}>
-        Essential links
-      </Text>
-      <Stack horizontal tokens={stackTokens} horizontalAlign="center">
-        <Link href="https://developer.microsoft.com/en-us/fluentui#/get-started/web">Docs</Link>
-        <Link href="https://stackoverflow.com/questions/tagged/office-ui-fabric">Stack Overflow</Link>
-        <Link href="https://github.com/microsoft/fluentui/">Github</Link>
-        <Link href="https://twitter.com/fluentui">Twitter</Link>
-      </Stack>
-      <Text variant="large" styles={boldStyle}>
-        Design system
-      </Text>
-      <Stack horizontal tokens={stackTokens} horizontalAlign="center">
-        <Link href="https://developer.microsoft.com/en-us/fluentui#/styles/web/icons">Icons</Link>
-        <Link href="https://developer.microsoft.com/en-us/fluentui#/styles/web">Styles</Link>
-        <Link href="https://aka.ms/themedesigner">Theme designer</Link>
-      </Stack>
-      </Stack>
+      </div>
     </Stack>
   );
 };
